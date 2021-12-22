5818b60ebd1fc1e3fde6982c897ee4eff7831a96
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-12-21 21:51:21 -0600

    variables typescript

--- intro/src/index.ts
+++ intro/src/index.ts
@@ -6,3 +6,36 @@ console.log('Hola Mundo!');
     ===== Código de TypeScript =====
 */
 
+
+
+
+/********************************/
+//    Variables en Typescript
+/********************************/
+
+let nombre: string  = 'Jesus';
+let edad:   number  = 33;
+let valor:  boolean = false;
+
+console.log( `Hola soy ${nombre} mi edad es ${edad}` );
+
+
+/********************************/
+//            
+/********************************/
+
+let arreglo: (string | number | boolean )[]= [];
+
+interface Personaje{
+
+    nombre:       string;
+    hp:           number;
+    habilidades:  string[];
+
+}
+
+const personaje: Personaje = {
+    nombre: 'Jesus',
+    hp: 100,
+    habilidades: [ 'fuego', 'agua', 'rayo' ]
+}
