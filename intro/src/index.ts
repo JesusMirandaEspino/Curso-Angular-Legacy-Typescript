b29d4169976315e878a3ab92a25fec397f5fc7b3
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-12-23 08:47:00 -0600

    Objetos e interfaces

--- intro/src/index.ts
+++ intro/src/index.ts
@@ -31,13 +31,17 @@ interface Personaje{
     nombre:       string;
     hp:           number;
     habilidades:  string[];
+    mostrarHp: () => void;
 
 }
 
 const personaje: Personaje = {
     nombre: 'Jesus',
     hp: 100,
-    habilidades: [ 'fuego', 'agua', 'rayo' ]
+    habilidades: [ 'fuego', 'agua', 'rayo' ],
+    mostrarHp(){
+        console.log( personaje.hp );
+    }
 }
 
 
@@ -55,6 +59,11 @@ const multiplicar = (a: number, b: number): number => {
 }
 
 
+const curar = ( personaje: Personaje, curarhp:number ) => {
+    personaje.hp += curarhp
+}
+
+
 console.log('*********************************************');
 console.log( sumar(5,10) );
 console.log('*********************************************');
@@ -62,3 +71,34 @@ console.log('*********************************************');
 console.log('*********************************************');
 console.log( multiplicar(5,10) );
 console.log('*********************************************');
+
+
+
+
+/********************************/
+//      Arreglos e interfaces      
+/********************************/
+
+interface SuperHeroe{
+    nombre: string;
+    edad: number;
+    direccion: {
+        calle: string;
+        pais: string
+        ciudad: string
+    },
+    MostrarDireccion: () => string;
+}
+
+const superHeroe = {
+    nombre: 'spiderman',
+    edad: 30,
+    direccion: {
+        calle: 'Main st',
+        pais: 'USA',
+        ciudad: 'NY'
+    },
+    MostrarDireccion() {
+        return `${this.nombre}  ${this.direccion}  ${this.direccion.pais}`
+    }
+}
