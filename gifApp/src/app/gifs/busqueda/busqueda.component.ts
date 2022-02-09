4c8c65e99004a48b49681b64e6e35177f352ba4c
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2022-02-08 21:12:44 -0600

    design components

---
+++ gifApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.scss']
+})
+export class BusquedaComponent implements OnInit {
+
+  constructor() {
+    //code
+  }
+
+  ngOnInit(): void {
+    // code
+  }
+
+}
---
+++ gifApp/src/app/gifs/resultados/resultados.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+  styleUrls: ['./resultados.component.scss']
+})
+export class ResultadosComponent implements OnInit {
+
+  constructor() {
+    //code
+  }
+
+  ngOnInit(): void {
+    //code
+  }
+
+}
