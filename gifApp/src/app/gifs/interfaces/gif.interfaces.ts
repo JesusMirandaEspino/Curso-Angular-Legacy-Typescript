2c2c926f600bfacebc8fdcb995c1327629d0422d
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2022-02-15 21:10:40 -0600

    interfaces

---
+++ gifApp/src/app/gifs/interfaces/gif.interfaces.ts
@@ -0,0 +1,108 @@
+export interface SearchResponse {
+    data: Datum[];
+}
+
+export interface Datum {
+    type:                       string;
+    id:                         string;
+    url:                        string;
+    slug:                       string;
+    bitly_gif_url:              string;
+    bitly_url:                  string;
+    embed_url:                  string;
+    username:                   string;
+    source:                     string;
+    title:                      string;
+    rating:                     string;
+    content_url:                string;
+    source_tld:                 string;
+    source_post_url:            string;
+    is_sticker:                 number;
+    import_datetime:            Date;
+    trending_datetime:          string;
+    images:                     Images;
+    user:                       User;
+    analytics_response_payload: string;
+    analytics:                  Analytics;
+}
+
+export interface Analytics {
+    onload:  Onclick;
+    onclick: Onclick;
+    onsent:  Onclick;
+}
+
+export interface Onclick {
+    url: string;
+}
+
+export interface Images {
+    original:                 FixedHeight;
+    downsized:                WillData;
+    downsized_large:          WillData;
+    downsized_medium:         WillData;
+    downsized_small:          DownsizedSmall;
+    downsized_still:          WillData;
+    fixed_height:             FixedHeight;
+    fixed_height_downsampled: FixedHeight;
+    fixed_height_small:       FixedHeight;
+    fixed_height_small_still: WillData;
+    fixed_height_still:       WillData;
+    fixed_width:              FixedHeight;
+    fixed_width_downsampled:  FixedHeight;
+    fixed_width_small:        FixedHeight;
+    fixed_width_small_still:  WillData;
+    fixed_width_still:        WillData;
+    looping:                  Looping;
+    original_still:           WillData;
+    original_mp4:             DownsizedSmall;
+    preview:                  DownsizedSmall;
+    preview_gif:              WillData;
+    preview_webp:             WillData;
+    "WillData":             WillData;
+}
+
+export interface WillData {
+    height: string;
+    width:  string;
+    size:   string;
+    url:    string;
+}
+
+export interface DownsizedSmall {
+    height:   string;
+    width:    string;
+    mp4_size: string;
+    mp4:      string;
+}
+
+export interface FixedHeight {
+    height:    string;
+    width:     string;
+    size:      string;
+    url:       string;
+    mp4_size?: string;
+    mp4?:      string;
+    webp_size: string;
+    webp:      string;
+    frames?:   string;
+    hash?:     string;
+}
+
+export interface Looping {
+    mp4_size: string;
+    mp4:      string;
+}
+
+export interface User {
+    avatar_url:    string;
+    banner_image:  string;
+    banner_url:    string;
+    profile_url:   string;
+    username:      string;
+    display_name:  string;
+    description:   string;
+    instagram_url: string;
+    website_url:   string;
+    is_verified:   boolean;
+}
