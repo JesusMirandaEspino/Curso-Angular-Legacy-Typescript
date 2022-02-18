a10fc694e37e9f2c3eb279df760052dca6b7227c
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2022-02-17 21:53:01 -0600

    Routers

--- countriesApp/src/app/app-routing.module.ts
+++ countriesApp/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BycapitalComponent } from './countries/pages/bycapital/bycapital.component';
+import { BycountryComponent } from './countries/pages/bycountry/bycountry.component';
+import { ByregionComponent } from './countries/pages/byregion/byregion.component';
+import { SeecountryComponent } from './countries/pages/seecountry/seecountry.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: BycountryComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'region',
+    component: ByregionComponent,
+  },
+  {
+    path: 'capital',
+    component: BycapitalComponent,
+  },
+  {
+    path: 'country/:id',
+    component: SeecountryComponent,
+
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
