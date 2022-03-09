b78bf142d5b298272fbbd1f8904a587438f9d57f
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2022-03-08 21:35:34 -0600

    animated

--- countriesApp/src/app/countries/components/countries-table/countries-table.component.ts
+++ countriesApp/src/app/countries/components/countries-table/countries-table.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
-
+import 'animate.css';
 @Component({
   selector: 'app-countries-table',
   templateUrl: './countries-table.component.html',
