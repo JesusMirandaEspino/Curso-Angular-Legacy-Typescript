65c7e685bca289d3a819d0a8b6501dc7df11cdb3
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-04-08 20:53:33 -0500

    sidenav

--- heroesApp/src/app/heroes/heroes.module.ts
+++ heroesApp/src/app/heroes/heroes.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { EditarComponent } from './pages/editar/editar.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -21,7 +23,9 @@ import { HeroesRoutingModule } from './heroes-routing.module';
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
