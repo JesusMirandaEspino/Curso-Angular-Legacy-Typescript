4217378941e3bf86df947207784c3a5e37dba740
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-04-11 21:39:08 -0500

    flex layout

--- heroesApp/src/app/material/material.module.ts
+++ heroesApp/src/app/material/material.module.ts
@@ -6,7 +6,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
-
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [],
   imports: [
@@ -18,7 +18,8 @@ import {MatListModule} from '@angular/material/list';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
