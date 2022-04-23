10b1d2601397adead51efa6384aa43e6325e418f
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-04-22 20:07:51 -0500

    aditar heroe page

--- heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interface/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  public publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    },
+
+  ];
+
+  public heroe: Heroe = {
+    superhero:        '',
+    publisher:        Publisher.DCComics,
+    alter_ego:        '',
+    first_appearance: '',
+    characters:       '',
+  }
+
+  constructor() {
+    // code
+  }
 
   ngOnInit(): void {
+    // code
   }
 
 }
