301b590f9481b45b2ccd72ec59576a5ac8761a13
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-05-11 21:10:11 -0500

    form ngmodel

--- formApp/src/app/template/basicos/basicos.component.ts
+++ formApp/src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    //  code
+  }
 
   ngOnInit(): void {
+    // code
   }
+
+guardar(miform:NgForm){
+  console.log( miform );
+}
+
 
 }
