5d5ca8b0ac64769364d1a41b64895a018ceffb9a
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-05-23 21:34:32 -0500

    validate reactive dinamics

--- formApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ formApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+
+miFormulario: FormGroup  = this.fb.group({
+  nombre: [ 'Yisus', [Validators.required, Validators.minLength(3)  ] ],
+  });
+
+
+  constructor( private fb: FormBuilder) {
+    // code
+  }
 
   ngOnInit(): void {
+    // code
   }
+
+  nombreValido(campo: string){
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
+  }
+
+
+  guardar(){
+
+    if( this.miFormulario.invalid ){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+      this.miFormulario.reset();
+
+
+  }
+
 
 }
