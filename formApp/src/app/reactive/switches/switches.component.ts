97d5c3c045802771e28f28e73584d5136bab64da
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-05-25 21:31:06 -0500

    git form switch

--- formApp/src/app/reactive/switches/switches.component.ts
+++ formApp/src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup  = this.fb.group({
+    radioGenero: [ 'm', Validators.required ],
+    notificaciones: [ true, Validators.required ],
+    terminosCondiciones: [ false, Validators.requiredTrue ]
+  });
+
+
+  public persona = {
+    radioGenero: 'f',
+    notificaciones: true
+  }
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset( {...this.persona, terminosCondiciones: true } );
   }
 
 }
