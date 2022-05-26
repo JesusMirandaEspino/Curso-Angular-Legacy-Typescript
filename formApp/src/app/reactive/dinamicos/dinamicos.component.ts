cfa7be5582c30f4cbe9354e052b10021d63afa4e
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-05-25 20:52:49 -0500

    delete favorito

--- formApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ formApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -16,8 +16,8 @@ get favoritosArray(){
 miFormulario: FormGroup  = this.fb.group({
   nombre: [ 'Yisus', [Validators.required, Validators.minLength(3)  ] ],
   favoritos: this.fb.array( [
-      ['The witcher']
-      ], [Validators.required] )
+      ['The witcher', Validators.required]
+      ], Validators.required )
   });
 
   nuevoFavorito: FormControl = this.fb.control( '', Validators.required );
@@ -36,11 +36,16 @@ miFormulario: FormGroup  = this.fb.group({
   }
 
   agregarFavorito(){
+
     if( this.nuevoFavorito.invalid ){ return }
     this.favoritosArray.push( this.fb.control( this.nuevoFavorito.value, Validators.required) );
 
     this.nuevoFavorito.reset();
+  }
 
+
+  borrar(index:number){
+    this.favoritosArray.removeAt( index);
   }
 
 
