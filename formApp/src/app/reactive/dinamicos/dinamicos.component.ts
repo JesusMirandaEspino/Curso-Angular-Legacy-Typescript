54f7709032c370c1fbcbb87a2393f918d84df4ab
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-05-25 21:13:21 -0500

    bug resolve null in array

--- formApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ formApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -8,19 +8,22 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 })
 export class DinamicosComponent implements OnInit {
 
-get favoritosArray(){
-  return this.miFormulario.get('favoritos') as FormArray;
-}
-
 
 miFormulario: FormGroup  = this.fb.group({
-  nombre: [ 'Yisus', [Validators.required, Validators.minLength(3)  ] ],
+  nombre: [ '', [Validators.required, Validators.minLength(3)  ] ],
   favoritos: this.fb.array( [
-      ['The witcher', Validators.required]
+      ['The witcher', Validators.required],
+      ['The witcher', Validators.required],
       ], Validators.required )
   });
 
+
   nuevoFavorito: FormControl = this.fb.control( '', Validators.required );
+
+
+get favoritosArray(){
+  return this.miFormulario.get('favoritos') as FormArray;
+}
 
 
   constructor( private fb: FormBuilder) {
@@ -36,7 +39,7 @@ miFormulario: FormGroup  = this.fb.group({
   }
 
   agregarFavorito(){
-
+    console.log(  this.favoritosArray);
     if( this.nuevoFavorito.invalid ){ return }
     this.favoritosArray.push( this.fb.control( this.nuevoFavorito.value, Validators.required) );
 
