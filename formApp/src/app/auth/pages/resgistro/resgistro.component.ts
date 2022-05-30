1c76257ec1271390086ed85a158dd298cb270167
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-05-30 09:23:25 -0500

    email validated

--- formApp/src/app/auth/pages/resgistro/resgistro.component.ts
+++ formApp/src/app/auth/pages/resgistro/resgistro.component.ts
@@ -9,15 +9,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ResgistroComponent implements OnInit {
 
   public nombreApellido: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public emailVliadated: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
   public miform: FormGroup = this.fb.group({
     nombre: [ '', [ Validators.required, Validators.pattern( this.nombreApellido ) ] ],
+    email: [ '', [ Validators.required, Validators.pattern( this.emailVliadated ) ] ],
 
   });
 
   constructor( private fb: FormBuilder  ) { }
 
   ngOnInit(): void {
+
+    this.miform.reset({
+      nombre: 'Jesus Miranda',
+      email: 'Jesus@Miranda.com',
+
+    });
+
+
   }
 
   campoValido(campo:string){
