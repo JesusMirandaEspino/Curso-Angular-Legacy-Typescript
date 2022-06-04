f5c842f2c46362c29c233f89bc4c36b2f90d2e82
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-03 19:27:54 -0500

    validation email form

--- formApp/src/app/auth/pages/resgistro/resgistro.component.ts
+++ formApp/src/app/auth/pages/resgistro/resgistro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ValidatorsService } from '../../../shared/validators/validators.service';
 import { emailVliadatedpattern, nombreApellidopattern, noPuedeSerStride } from '../../../shared/validators/validators';
+import { EmailValidatorsService } from 'src/app/shared/validators/email-validators.service';
 
 @Component({
   selector: 'app-resgistro',
@@ -14,7 +15,7 @@ export class ResgistroComponent implements OnInit {
 
   public miform: FormGroup = this.fb.group({
     nombre: [ '', [ Validators.required, Validators.pattern( this.validatorsService.nombreApellidopattern ) ] ],
-    email: [ '', [ Validators.required, Validators.pattern( this.validatorsService.emailVliadatedpattern ) ] ],
+    email: [ '', [ Validators.required, Validators.pattern( this.validatorsService.emailVliadatedpattern ) ], [this.emailValidatorsService] ],
     username: [ '', [ Validators.required, this.validatorsService.noPuedeSerStride ] ],
     pass1: [ '', [ Validators.required, Validators.minLength(6) ] ],
     pass2: [ '', [ Validators.required ] ],
@@ -22,13 +23,15 @@ export class ResgistroComponent implements OnInit {
     validators: [ this.validatorsService.camposIguales( 'pass1', 'pass2' ) ]
   });
 
-  constructor( private fb: FormBuilder, private validatorsService: ValidatorsService  ) { }
+  constructor(  private fb: FormBuilder,
+                private validatorsService: ValidatorsService,
+                private emailValidatorsService: EmailValidatorsService   ) { }
 
   ngOnInit(): void {
 
     this.miform.reset({
       nombre: 'Jesus Miranda',
-      email: 'Jesus@Miranda.com',
+      email: 'test1@test.com',
 
     });
   }
