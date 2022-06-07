2af16c39f3763984308b48b7dc8347488da3d153
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-06 20:39:00 -0500

    selectores basic pages

---
+++ selectores/src/app/paises/pages/selector-pages/selector-pages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-pages',
+  templateUrl: './selector-pages.component.html',
+  styleUrls: ['./selector-pages.component.scss']
+})
+export class SelectorPagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
