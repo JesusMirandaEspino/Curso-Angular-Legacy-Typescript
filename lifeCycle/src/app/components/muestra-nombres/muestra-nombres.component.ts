3f8cf77299f467ed885606b1224fd6460e410406
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-13 20:52:18 -0500

    lifecycle console

--- lifeCycle/src/app/components/muestra-nombres/muestra-nombres.component.ts
+++ lifeCycle/src/app/components/muestra-nombres/muestra-nombres.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MuestraNombresComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+
   }
 
 }
--- lifeCycle/src/app/pages/pagina1/pagina1.component.ts
+++ lifeCycle/src/app/pages/pagina1/pagina1.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pagina1',
   templateUrl: './pagina1.component.html',
   styleUrls: ['./pagina1.component.scss']
 })
-export class Pagina1Component implements OnInit {
+export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    console.log('constructor');
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(){
+    console.log( 'ngOnChanges' );
+  }
+
+
+  ngDoCheck(){
+    console.log( 'ngDoCheck' );
+  }
+
+  ngAfterContentInit(){
+    console.log( 'ngAfterContentInit' );
+  }
+
+
+  ngAfterViewInit(){
+    console.log( 'ngAfterViewInit' );
+  }
+
+  ngAfterViewChecked(){
+    console.log( 'ngAfterViewChecked' );
+  }
+
+  ngOnDestroy(){
+    console.log( 'ngOnDestroy' );
   }
 
 }
