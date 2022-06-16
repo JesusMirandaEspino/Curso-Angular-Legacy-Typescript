1e1b7a733f1ea835c4577da23550002766494917
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-15 20:58:40 -0500

    graph components

---
+++ graficasApp/src/app/graphics/components/graphics-bar/graphics-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphics-bar',
+  templateUrl: './graphics-bar.component.html',
+  styleUrls: ['./graphics-bar.component.scss']
+})
+export class GraphicsBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ graficasApp/src/app/graphics/graphics.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraphicsRoutingModule } from './graphics-routing.module';
+import { BarsComponent } from './pages/bars/bars.component';
+import { BarsDoubleComponent } from './pages/bars-double/bars-double.component';
+import { DonaComponent } from './pages/dona/dona.component';
+import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
+import { GraphicsBarComponent } from './components/graphics-bar/graphics-bar.component';
+
+
+@NgModule({
+  declarations: [
+    BarsComponent,
+    BarsDoubleComponent,
+    DonaComponent,
+    DonaHttpComponent,
+    GraphicsBarComponent
+  ],
+  imports: [
+    CommonModule,
+    GraphicsRoutingModule
+  ]
+})
+export class GraphicsModule { }
