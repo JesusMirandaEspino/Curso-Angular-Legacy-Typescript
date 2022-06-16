3696ea211cd231c9828708fd773d3258c5747024
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-15 21:20:36 -0500

    router

--- graficasApp/src/app/graphics/graphics-routing.module.ts
+++ graficasApp/src/app/graphics/graphics-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarsDoubleComponent } from './pages/bars-double/bars-double.component';
+import { BarsComponent } from './pages/bars/bars.component';
+import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
+import { DonaComponent } from './pages/dona/dona.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    children: [
+                {
+                  path: 'bar',
+                  component: BarsComponent
+                },
+                {
+                  path: 'bardouble',
+                  component: BarsDoubleComponent
+                },
+                {
+                  path: 'dona',
+                  component: DonaComponent
+                },
+                {
+                  path: 'donaHttp',
+                  component: DonaHttpComponent
+                },
+                {
+                  path: '**',
+                  redirectTo: 'bar'
+                }
+              ]
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
