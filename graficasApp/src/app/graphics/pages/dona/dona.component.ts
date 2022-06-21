8a3731c5ea9bf3d341d1a2149381a008af6eab7c
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-20 20:46:36 -0500

    dona

--- graficasApp/src/app/graphics/pages/dona/dona.component.ts
+++ graficasApp/src/app/graphics/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartEvent, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonaComponent implements OnInit {
 
-  constructor() { }
+// Doughnut
+  public doughnutChartLabels: string[] = [ 'D' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ], backgroundColor: ['#6405F0','#0724E3', '#05A0F0']  },
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+
+
+  constructor() {
+    // code
+  }
 
   ngOnInit(): void {
+    // code
   }
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
 
 }
