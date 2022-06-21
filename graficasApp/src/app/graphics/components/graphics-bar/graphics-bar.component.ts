d58eb371dfa37d8c6870e9acb47df9fc9c0cf2dc
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-20 20:27:53 -0500

    double bar

--- graficasApp/src/app/graphics/components/graphics-bar/graphics-bar.component.ts
+++ graficasApp/src/app/graphics/components/graphics-bar/graphics-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import { ChartConfiguration, ChartData,  ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 @Component({
   selector: 'app-graphics-bar',
   templateUrl: './graphics-bar.component.html',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphicsBarComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  @Input() barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+      }
+    }
+  };
+
+  @Input() barChartType: ChartType = 'bar';
+
+
+
+  @Input() barChartData: ChartData = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' },
+      { data: [ 82, 28, 1, 39, 46, 67, 50 ], label: 'Series C' }
+
+    ]
+  };
+
+
+  constructor() {
+    // code
+  }
 
   ngOnInit(): void {
+    // code
   }
+
+public randomize(): void {
+
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      Math.round(Math.random() * 100),
+      56,
+      Math.round(Math.random() * 100),
+      40 ];
+
+    this.chart?.update();
+  }
+
+
 
 }
--- graficasApp/src/app/graphics/pages/bars-double/bars-double.component.ts
+++ graficasApp/src/app/graphics/pages/bars-double/bars-double.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ChartConfiguration, ChartData,  ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 @Component({
   selector: 'app-bars-double',
   templateUrl: './bars-double.component.html',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarsDoubleComponent implements OnInit {
 
-  constructor() { }
+
+    proveedoresData: ChartData<'line'> = {
+      labels: ['2021', '2022','2023','2024','2025'],
+      datasets:[
+        { data: [ 100,50,300,200,500 ], label: 'Vendedor A',
+            backgroundColor: 'lightgreen',
+            borderColor: 'green',
+            pointBackgroundColor: 'lightgreen',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)', },
+        { data: [ 50,250,30, 450,200 ], label: 'Vendedor B' }
+      ]
+    }
+
+    productoData: ChartData<'bar'> = {
+      labels: ['2021', '2022','2023','2024','2025'],
+      datasets:[
+        { data: [ 200, 300,400,300, 100 ], label: 'Carros', backgroundColor: 'blue' }
+      ]
+    }
+
+
+    lineOptions: ChartConfiguration['options'] = {
+      elements: {
+        line: {
+          tension: 0.5
+        }
+      }
+    }
+
+
+  constructor() {
+    // code
+  }
 
   ngOnInit(): void {
+    // code
   }
 
 }
