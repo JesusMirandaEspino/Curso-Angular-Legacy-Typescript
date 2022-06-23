50de12bbd6ecae08abc5ab7351bbbd24245047c6
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-22 21:01:46 -0500

    directivas

--- directivas/src/app/products/pages/agregar/agregar.component.ts
+++ directivas/src/app/products/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  public miform: FormGroup = this.fb.group({
+    nombre: ['', Validators.required]
+  });
+
+  constructor( private fb: FormBuilder) {
+    // code
+  }
 
   ngOnInit(): void {
+    // code
+  }
+
+  tieneError(_campo: string){
+    return this.miform.get( _campo )?.invalid || false;
   }
 
 }
--- directivas/src/app/products/products.module.ts
+++ directivas/src/app/products/products.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { AgregarComponent } from './pages/agregar/agregar.component';
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductsModule { }
