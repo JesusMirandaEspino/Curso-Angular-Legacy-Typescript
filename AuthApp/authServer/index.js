b7a9550dcf7d76015f7f539b7155916bf7a9473d
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-06-29 20:40:43 -0500

    post & get

--- AuthApp/authServer/index.js
+++ AuthApp/authServer/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 
 // Rutas
-app.use( 'api/auth', require('./routers/auth') );
+app.use( '/api/auth', require('./routers/auth') );
 
 app.listen(4000, () => {
     console.log(`Servidor corriendo en puerto ${4000}`);
