ab48e6bbc27b648514b0e019ea4850ade532da28
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-11 13:05:20 -0500

    lazy load

--- AuthApp/authApp/src/app/protected/protected-routing.module.ts
+++ AuthApp/authApp/src/app/protected/protected-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DahsboardComponent } from './dahsboard/dahsboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+                {
+                  path: '',
+                  component: DahsboardComponent,
+                },
+                {
+                  path: '**',
+                  redirectTo: ''
+                }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
