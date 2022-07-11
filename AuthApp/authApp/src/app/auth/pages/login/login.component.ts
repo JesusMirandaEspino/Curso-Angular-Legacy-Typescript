88bd43922dbc641926e1ccb3688bfa0070addf99
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-11 13:27:30 -0500

    basic form

--- AuthApp/authApp/src/app/auth/pages/login/login.component.ts
+++ AuthApp/authApp/src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup  = this.fb.group({
+    email: [ 'mitest1@gmail.com', [ Validators.required, Validators.email ] ],
+    password: [ '123456', [ Validators.required, Validators.min(6) ] ]
+  });
+
+  constructor( private fb: FormBuilder ) {
+    // code
+  }
 
   ngOnInit(): void {
+    // code
+  }
+
+  login(){
+    console.log('hola');
   }
 
 }
--- AuthApp/authApp/src/app/auth/pages/register/register.component.ts
+++ AuthApp/authApp/src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+
+  miFormulario: FormGroup  = this.fb.group({
+    nombre: [ 'jesus', [ Validators.required ] ],
+    email: [ 'mitest1@gmail.com', [ Validators.required, Validators.email ] ],
+    password: [ '123456', [ Validators.required, Validators.min(6) ] ],
+    password2: [ '123456', [ Validators.required, Validators.min(6) ] ],
+  });
+
+  constructor( private fb: FormBuilder ) {
+    // code
+  }
 
   ngOnInit(): void {
+    // code
+  }
+
+
+  login(){
+    console.log(this.miFormulario);
   }
 
 }
